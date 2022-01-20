ec559f08138a0476e277c715f035386061976177
Jonny Kelly <jonny.kelly192@gmail.com>
2022-01-20 14:21:42 +0000

    allows user to mint membership nft with button

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,20 @@ const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("Address:", address)
 
+  // Signer is require to sign transactions on the blockchain.
+  // Without this we can only read data, not write.
+  const signer = provider ? provider.getSigner() : undefined;
+
+  // Keeps a loading state while NFT is minting.
+  const [isClaiming, setIsClaiming] = useState(false)
+
   // State variable to check if user has our NFT
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(() => {
+    // Pass the signer to the SDK enabling us to interact with our deployed contract
+    sdk.setProviderOrSigner(signer);
+  }, [signer]);
 
   // If they don't have a connected wallet
   useEffect(() => {
@@ -57,14 +69,45 @@ const App = () => {
       </div>
     );
   }
-  
-// Where a user has their wallet connected
-return (
-  <div className="landing">
-    <h1> Wallet connected...</h1>
-  </div>
-)
 
+  // If user has claimed membership NFT
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>DAO Members Page</h1>
+        <p>Welcome to the DebugDAO</p>
+      </div>
+    )
+  }
+
+  const mintNft = () => {
+    setIsClaiming(true);
+    bundleDropModule
+    .claim("0", 1)
+    .then (() => {
+      setHasClaimedNFT(true);
+      console.log(`Successfully minted. Check it out on OpenSea: https://testnets.opensea.io/assets/${bundleDropModule.address}/0`);
+    })
+    .catch((err) => {
+      console.error("failed to claim", err);
+    })
+    .finally(() => {
+      setIsClaiming(false);
+    })
+  }
+  
+// Render mint NFT screen
+return (
+  <div className="mint-nft">
+    <h1> Mint your free DAO membership NFT</h1>
+    <button
+      disabled={isClaiming}
+      onClick={() => mintNft()}
+      >
+        {isClaiming ? "Minting..." : "Mint your NFT for free"}
+    </button>
+  </div>
+  )
 };
 
 export default App;
